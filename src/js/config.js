d3c672708fa7caf40c5c368325d9628a2e1f26ec
Sama-120 <sama.cmp@gmail.com>
2025-05-26 00:21:20 +0330

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 30;
+export const RES_PER_PAGE = 10;
+export const KEY = '48a52f58-4d2d-4995-9d76-7c200e8cd06c';
+export const MODAL_CLOSE_SEC = 2.5;
